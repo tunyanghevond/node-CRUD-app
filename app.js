33e4bbba0e4d28672aeace7e4f6ad5f78623b562
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-10-31 18:49:47 +0100

    connect app to db and add crud functionality in controllers

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+
 const connectDB = require('./db/connect')
+
 require('dotenv').config()
+
 app.use(express.json())
 
 app.use('/api/v1/tasks', tasks)
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,17 +1,49 @@
-const getAllTasks = (req, res) => {
-  res.send('Get all tasks')
+const Task = require('../models/Task')
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const createTask = (req, res) => {
-  res.json(req.body)
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findOne({ _id: taskId })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+
   res.json({ id: req.params.id })
 }
 const updateTask = (req, res) => {
   res.send('update task')
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findByIdAndDelete({ _id: taskId })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
