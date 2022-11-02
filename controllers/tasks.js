e025fd32adcc8a0fb9df602f9c27789b15bf4374
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-11-02 17:12:10 +0100

    connect app to db and add crud functionality in controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -30,9 +30,6 @@ const getTask = async (req, res) => {
 
   res.json({ id: req.params.id })
 }
-const updateTask = (req, res) => {
-  res.send('update task')
-}
 const deleteTask = async (req, res) => {
   try {
     const { id: taskId } = req.params
@@ -46,4 +43,19 @@ const deleteTask = async (req, res) => {
   }
 }
 
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
